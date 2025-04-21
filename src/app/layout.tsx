b555a2e1289a9dfe55831d40bb53a9873909c15b
GEEG <e.glebov@innopolis.university>
2025-04-21 19:30:21 +0300

    Minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <meta name="author" content="Egor Glebov" />
+      <title>Egor Glebov</title>
     </head>
       <body className={inter.className}>
         {/* <Header /> */}
